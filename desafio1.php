88a5faec807689c2004df7cf36dca70e72ce58dc
Joaomlq <132737162+Joaomlq@users.noreply.github.com>
2023-09-12 13:41:52 -0300

    Add files via upload

---
+++ desafio1.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="estilosif.css">
+</head>
+<body>
+    <div class="mae">
+  <div class="cabecalho">
+    <h1>Controle de frotas Conda Hivic</h1>
+  </div>
+  <div class="login">
+    <p>Escolha o tipo de login:</p>
+    <ul>
+        <li><a href="login-admin.html">Administrador</a></li>
+        <li><a href="login-usuario.html">Usuário</a></li>
+        <li><a href="login-motorista.html">Motorista</a></li>
+    </ul>
+  </div>
+  <div class="rodape">
+    <h3>Fale conosco!</h3>
+    <p>Ouvidoria principal +55 98478-0561</p>
+  </div>
+    </div>
+</body>
+</html>
+
+<?php
+
+?>
